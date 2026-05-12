1c010e561ceab24e0cbceb51995736dd611c0c59
SpencerBooth24 <Spencer_booth27@milton.edu>
2026-05-12 09:42:59 -0400

    push

--- src/Card.java
+++ src/Card.java
@@ -13,6 +13,39 @@ public Card(String pSuit,int pValue,int pName){
     } else if (pName==1) {
         name="Two";
         value=2;
+    } else if (pName==2) {
+        name="Three";
+        value=3;
+    }else if (pName==3) {
+        name="Four";
+        value=4;
+    } else if (pName==4) {
+        name="Five";
+        value=5;
+    }else if (pName==5) {
+        name="Six";
+        value=6;
+    } else if (pName==6) {
+        name="Seven";
+        value=7;
+    }else if (pName==7) {
+        name="Eight";
+        value=8;
+    } else if (pName==8) {
+        name="Nine";
+        value=9;
+    }else if (pName==9) {
+        name="Ten";
+        value=10;
+    } else if (pName==10) {
+        name="Jack";
+        value=10;
+    }else if (pName==11) {
+        name="Queen";
+        value=10;
+    } else if (pName==12) {
+        name="King";
+        value=10;
     }
     printInfo();
 }
